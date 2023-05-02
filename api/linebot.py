72b8d66c09de78f16ca1adbc571268b4fbe412c4
Michael Chen <michael1214c@gmail.com>
2023-05-03 03:50:28 +0800

    update file

--- api/linebot.py
+++ api/linebot.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselColumn,
-                            CarouselTemplate, MessageAction, URIAction)
+                            CarouselTemplate, MessageAction, URIAction, ImageCarouselColumn, ImageCarouselTemplate)
 import os
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
@@ -34,21 +34,28 @@ def callback():
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    if event.message.text == '教練資訊':
-        carousel_template = TemplateSendMessage(
-            alt_text = 'carousel template',
-            template = CarouselTemplate(
-                columns = [
-                    #第一個
-                    CarouselColumn(
-                        thumbnail_image_url = 'https://jimmy2130.github.io/WestGolf/images/GaryLuCrop.jpg'),
-                    #第二個
-                    CarouselColumn(
-                        thumbnail_image_url = 'https://jimmy2130.github.io/WestGolf/images/VincentLu.jpg')
-                ])
-            )
+    image_carousel_columns = [
+        ImageCarouselColumn(
+            image_url='https://example.com/image1.jpg',
+            action=MessageAction(label='Button 1', text='Button 1 pressed')
+        ),
+        ImageCarouselColumn(
+            image_url='https://example.com/image2.jpg',
+            action=MessageAction(label='Button 2', text='Button 2 pressed')
+        ),
+        ImageCarouselColumn(
+            image_url='https://example.com/image3.jpg',
+            action=MessageAction(label='Button 3', text='Button 3 pressed')
+        )
+    ]
+    image_carousel_template = ImageCarouselTemplate(columns=image_carousel_columns)
+    template_message = TemplateSendMessage(
+        alt_text='Image Carousel template',
+        template=image_carousel_template
+        )
 
-        line_bot_api.reply_message(event.reply_token, carousel_template)
+    if event.message.text == '教練資訊':
+        line_bot_api.reply_message(event.reply_token, template_message)
 
 
 if __name__ == "__main__":
