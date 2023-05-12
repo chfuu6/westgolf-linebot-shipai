63ab445991d76a364bd6695309c9c1f5ce22a534
Michael Chen <michael1214c@gmail.com>
2023-05-12 18:30:42 +0800

    update store info

--- api/linebot.py
+++ api/linebot.py
@@ -3,7 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselColumn,
                             CarouselTemplate, MessageAction, URIAction, ImageCarouselColumn, ImageCarouselTemplate,
-                            ImageSendMessage)
+                            ImageSendMessage, FlexSendMessage)
 import os
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
@@ -77,7 +77,150 @@ def handle_message(event):
     image_fee = ImageSendMessage(
                     original_content_url='https://i.imgur.com/KDwuzWN_d.webp?maxwidth=760&fidelity=grand',
                     preview_image_url='https://i.imgur.com/KDwuzWN_d.webp?maxwidth=760&fidelity=grand')
-
+    
+    flex_storeInfo = {
+        "type": "bubble",
+        "hero": {
+            "type": "image",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+            "size": "full",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover",
+            "action": {
+            "type": "uri",
+            "uri": "http://linecorp.com/"
+            }
+        },
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+                "type": "text",
+                "text": "石牌門市",
+                "weight": "bold",
+                "size": "xl"
+            },
+            {
+                "type": "box",
+                "layout": "vertical",
+                "margin": "lg",
+                "spacing": "sm",
+                "contents": [
+                {
+                    "type": "box",
+                    "layout": "baseline",
+                    "spacing": "sm",
+                    "contents": [
+                    {
+                        "type": "text",
+                        "text": "電話",
+                        "color": "#aaaaaa",
+                        "size": "sm",
+                        "flex": 1
+                    },
+                    {
+                        "type": "text",
+                        "text": "2828-7313",
+                        "wrap": true,
+                        "color": "#666666",
+                        "size": "sm",
+                        "flex": 5
+                    }
+                    ]
+                },
+                {
+                    "type": "box",
+                    "layout": "baseline",
+                    "spacing": "sm",
+                    "contents": [
+                    {
+                        "type": "text",
+                        "text": "地址",
+                        "color": "#aaaaaa",
+                        "size": "sm",
+                        "flex": 1
+                    },
+                    {
+                        "type": "text",
+                        "text": "台北市北投區承德路七段223之2號",
+                        "wrap": true,
+                        "color": "#666666",
+                        "size": "sm",
+                        "flex": 5
+                    }
+                    ]
+                },
+                {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                    {
+                        "type": "text",
+                        "text": "營業時間",
+                        "size": "sm",
+                        "color": "#aaaaaa",
+                        "flex": 1
+                    },
+                    {
+                        "type": "text",
+                        "text": "週一至週五 09:30-22:00",
+                        "size": "sm",
+                        "flex": 5,
+                        "color": "#666666",
+                        "offsetStart": "14px"
+                    },
+                    {
+                        "type": "text",
+                        "text": "週六、週日 08:00-19:00",
+                        "size": "sm",
+                        "flex": 5,
+                        "color": "#666666",
+                        "offsetStart": "14px"
+                    }
+                    ],
+                    "spacing": "sm"
+                }
+                ]
+            }
+            ]
+        },
+        "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+            {
+                "type": "button",
+                "style": "link",
+                "height": "sm",
+                "action": {
+                "type": "uri",
+                "label": "CALL",
+                "uri": "https://linecorp.com"
+                }
+            },
+            {
+                "type": "button",
+                "style": "link",
+                "height": "sm",
+                "action": {
+                "type": "uri",
+                "label": "WEBSITE",
+                "uri": "https://linecorp.com"
+                }
+            },
+            {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [],
+                "margin": "sm"
+            }
+            ],
+            "flex": 0
+        }
+    }
+    flex_message = FlexSendMessage(alt_text='門市資訊', contents=flex_storeInfo)
     
     businessHours = '西鈞高爾夫推廣中心\n\
 網址：https://jimmy2130.github.io/WestGolf/index.html\n\n\
@@ -118,7 +261,7 @@ Line ID：@298yqvcd (要加@)\n\
     # if event.message.text == '費用介紹':
     #     line_bot_api.reply_message(event.reply_token, image_fee)
     if event.message.text == '門市資訊':
-        line_bot_api.reply_message(event.reply_token, text_businessHours)
+        line_bot_api.reply_message(event.reply_token, flex_message)
     if event.message.text == '課程預約':
         line_bot_api.reply_message(event.reply_token, text_reservation)
 
